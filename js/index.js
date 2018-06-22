4294314c86e8177acd61045d83225ef83dc79543
QIANA <barton.qiana@gmail.com>
2018-06-22 13:29:53 -0700

    fix

--- js/index.js
+++ js/index.js
@@ -11,6 +11,7 @@ qb.App = (function() {
         $(window).resize(); //on page load
         //modal();
         form();
+        // mobileNav();
     }
 
     function sizing() {
@@ -66,6 +67,24 @@ qb.App = (function() {
     }
 
 
+    function mobileNav() {
+        //Menu button on click event
+        $('.mobile-nav-button').on('click', function() {
+            // Toggles a class on the menu button to transform the burger menu into a cross
+            $(".mobile-nav-button .mobile-nav-button__line:nth-of-type(1)").toggleClass("mobile-nav-button__line--1");
+            $(".mobile-nav-button .mobile-nav-button__line:nth-of-type(2)").toggleClass("mobile-nav-button__line--2");
+            $(".mobile-nav-button .mobile-nav-button__line:nth-of-type(3)").toggleClass("mobile-nav-button__line--3");
+
+            // Toggles a class that slides the menu into view on the screen
+            $('.mobile-menu').toggleClass('mobile-menu--open');
+            return false;
+        });
+
+        $('.mobile-btn').click(function(e) {
+            e.preventDefault
+            $(this).toggleClass('cross');
+        });
+    }
 
     /* ------------------------------------------------- */
     return {
